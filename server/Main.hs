1d422e09dd140c18392370a8bef14167aabea65b
Ben Millwood <thebenmachine+git@gmail.com>
2020-08-13 13:36:17 +0800

    start writing a server

---
+++ server/Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE NamedFieldPuns #-}
+module Main where
+
+import Control.Monad
+import GHC.Generics
+
+import qualified Data.Aeson as Aeson
+import qualified Network.WebSockets as WS
+
+main :: IO ()
+main = WS.runServer "localhost" 45286 app
+
+app :: WS.ServerApp
+app = handleConnection <=< WS.acceptRequest
+
+data LoginRequest = LoginRequest { username :: String } deriving (Generic, Show)
+
+instance Aeson.FromJSON LoginRequest where
+
+handleConnection :: WS.Connection -> IO ()
+handleConnection conn = do
+  login <- WS.receiveData conn
+  case Aeson.decode login of
+    Nothing -> error "decode failed"
+    Just (LoginRequest { username }) ->
+      error username
